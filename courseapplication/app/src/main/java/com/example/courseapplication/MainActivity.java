5078fbd69b24bfa9ba8c6835ba39bded3c5483ea
juhapii <juha.piippone@gmail.com>
2021-08-30 20:16:39 +0300

    Course project first page created

--- courseapplication/app/src/main/java/com/example/courseapplication/MainActivity.java
+++ courseapplication/app/src/main/java/com/example/courseapplication/MainActivity.java
@@ -1,43 +1,28 @@
 package com.example.courseapplication;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView myListView;
-    String[] items;
-    String[] prices;
-    String[] descriptions;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Resources res = getResources();
-        myListView = (ListView) findViewById(R.id.myListView);
-        items = res.getStringArray(R.array.items);
-        prices = res.getStringArray(R.array.prices);
-        descriptions = res.getStringArray(R.array.descriptions);
-
-        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
-        myListView.setAdapter(itemAdapter);
-
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Button itemListBtn = (Button) findViewById(R.id.itemListBtn);
+        itemListBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
-                showDetailActivity.putExtra("com.example.ITEM_INDEX", i);
-                startActivity(showDetailActivity);
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), itemListActivity.class);
+                //pass information to second activity TEST PURPOSE
+                startIntent.putExtra("org.example.courseapplication.SOMETHING", "Hello WORLD TEST");
+                startActivity(startIntent);
             }
         });
-        //myListView.setAdapter(new ArrayAdapter<String>(this, R.layout.my_listview_detail, items));
+
     }
 }
